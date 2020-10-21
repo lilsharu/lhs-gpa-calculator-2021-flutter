2cf163d9cbeabfb78273442e51ab4e3eff01c1de
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-21 11:02:45 -0400

    Starts a basic implementation of ClassNumber

--- lib/models/class_number.dart
+++ lib/models/class_number.dart
@@ -1,2 +1,20 @@
+import 'package:equatable/equatable.dart';
+
 // TODO Implement ClassNumber
-class ClassNumber {}
+class ClassNumber extends Equatable {
+  final int number;
+  final String addition;
+
+  ClassNumber(this.number, [this.addition = '']);
+
+  @override
+  List<Object> get props => [number, addition];
+
+  static ClassNumber parseNumber(String courseNumber) {
+    var number = int.parse(courseNumber.substring(0, 3));
+    var addition = '';
+    if (courseNumber.length > 3) addition = courseNumber.substring(3);
+
+    return ClassNumber(number, addition);
+  }
+}
