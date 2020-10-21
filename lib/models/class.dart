c50f36f69d78886e7a9bfb8f8e5042c2ee2e6931
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-20 22:24:26 -0400

    Adds all fields to Class
    Also Creates a shell for ClassNumber

--- lib/models/class.dart
+++ lib/models/class.dart
@@ -1,10 +1,27 @@
 import 'package:decimal/decimal.dart';
 
-enum Length {
+import './class_number.dart';
+import './department.dart';
+
+enum ClassLength {
   halfYear,
   fullYear,
 }
 
+enum ClassLevel {
+  CP,
+  Honors,
+  AP,
+}
+
 class Class {
+  String name;
+  Decimal credits;
+  ClassLevel level;
+  Department department;
+  ClassLength length;
+  ClassNumber classNumber;
+  bool isCore;
+
   Class.empty();
 }
---
+++ lib/models/class_number.dart
@@ -0,0 +1,2 @@
+// TODO Implement ClassNumber
+class ClassNumber {}
