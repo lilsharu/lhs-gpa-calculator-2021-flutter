a36390ec19649d5f4abad3a09ae5b568352e6227
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-22 17:17:38 -0400

    Merges Class and ClassNumber to one File
    This allows easy imports and reduced excess dependencies

--- lib/models/class.dart
+++ lib/models/class.dart
@@ -1,7 +1,7 @@
 import 'package:decimal/decimal.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
-import './class_number.dart';
 import './department.dart';
 
 class Class {
@@ -95,4 +95,37 @@ enum ClassLevel {
   CP,
   Honors,
   AP,
+}
+
+class ClassNumber extends Equatable {
+  final int _number;
+  final String _addition;
+
+  const ClassNumber(this._number, [this._addition = '']);
+
+  String get getAddition => _addition;
+
+  int get getNumber => _number;
+
+  @override
+  List<Object> get props => [_number, _addition];
+
+  @override
+  String toString() {
+    String numberPortion = _number.toString();
+
+    while (numberPortion.length < 3) {
+      numberPortion = '0$numberPortion';
+    }
+
+    return '$numberPortion$_addition';
+  }
+
+  static ClassNumber parseNumber(String courseNumber) {
+    var number = int.parse(courseNumber.substring(0, 3));
+    var addition = '';
+    if (courseNumber.length > 3) addition = courseNumber.substring(3);
+
+    return ClassNumber(number, addition);
+  }
 }
